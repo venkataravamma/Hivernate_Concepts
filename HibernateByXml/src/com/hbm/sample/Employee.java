9a63525a1579cb4dda0b47269d2c29fbfd4f5795
venky <v.venky9815@gmail.com>
2023-03-25 16:09:26 +0530

    hibernate by xml

---
+++ HibernateByXml/src/com/hbm/sample/Employee.java
@@ -0,0 +1,31 @@
+package com.hbm.sample;
+
+public class Employee {
+	private int id;
+	private String firstname, lastname;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+}
